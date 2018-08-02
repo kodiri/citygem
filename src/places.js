5d75f32931ff4d0e8b4da5d47394f470f1b9279c
Ackeem <damanybailey@DamanyBileysMBP.localdomain>
2018-08-02 16:37:39 +0100

    array of palces

---
+++ src/places.js
@@ -0,0 +1,131 @@
+const Places = [
+{
+    id: "things-to-know-about-holland-parks-kyoto-garden",
+    name: "Kyoto-Gardens",
+    website:"https://www.parkgrandkensington.co.uk/blog/things-to-know-about-holland-parks-kyoto-garden/",
+    address:"Holland Park, Holland Park Ave, Kensington, London W11 4UA",
+    description:"The Kyoto Japanese Garden is a hidden gem wrapped in another hidden gem: Holland Park. The beautiful park is tucked away in smart Kensington and has plenty of its own hidden corners, with winding paths, statues, peacocks, an opera house and an orangery, alongside the tranquil Kyoto Gardens",
+    category:['gardens', 'theme'],
+},
+
+{
+    id: "Whats-on-Detectives",
+    name: "Evans & Peel Detective Agency",
+    website:"http://www.evansandpeel.com/Whats-on-Detectives",
+    address:"Underneath Juice Club, 28 Bedfordbury, London WC2N 4RB,",
+    description:"This Earl's Court dive masquerades as a detective agency online. In order to make a reservation, you must subit a 'case', which the 'detective' (or doorman) will question you about on arrival. The true excitement is being led through a secret bookcase and into the candleit 1920s style bar; the drinks, are just as spectacular. Try the 'Cold Fashioned', a sweeter, nutty, vintage take on an Old Fashioned. A guaranteed fun night if you enjoy theatrics with your drinks",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "The-Luggage-Room",
+    name: "The Luggage Room",
+    website:"http://luggageroom.co.uk/",
+    address:"The Luggage Room Grosvenor Square London W1K 6JP",
+    description:"Tucked away in the corner of Grosvenor Square, the Luggage Room draws on the spirit of the Roaring Twenties. It’s an intimate London cocktail bar, effortlessly oozing style and decadence. Amid the fine liquor displays, handmade wine cabinets and period curiosities, you can watch while all nature of libations are carefully created around you.",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "The-Mayor-of-Scaredy-Cat-Town",
+    name: "The Mayor of Scaredy Cat Town",
+    website:"http://www.themayorofscaredycattown.com/",
+    address:"12 Artillery Lane, London E1 7LS, England",
+    description:"Think that your fridge is for discarded old cheeses and stale bottles of milk? While yes would be our regular answer, there's one secret bar in the city championing a new way to look at fridges. Hidden behind a Smeg fridge, and accessible thanks to a cheeky request to see 'The Mayor' at the Breakfast Club bar, descend the stairs into a basement filled with fun, food and great drinks. A broody basement of vintage furniture and Canadian whisky lodge meets speakeasy, Mayor of Scaredy Cat Town is an elusive romp away from the bustle of upstairs.",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "about-us",
+    name: "The Castle Cinema",
+    website:"https://thecastlecinema.com/about-us/",
+    address:"64 Brooksby's Walk London E9 6DA",
+    description:"Since it opened in 1913, The Castle in Clapton has been a cinema, a bingo hall, a shoe factory and a snooker club. Now, after raising £57,000 on Kickstarter, a local couple has restored it back to its former filmic glory. And it’s gorgeous. There’s an art deco bar – all elegant marble tables and velvet cocktail chairs. The boutique cinema is a bit of a stunner too, with a petite screen, 80 retro hotel seats and a curved ceiling covered in ornate gold plasterwork that’ll make your face look like the heart-eye emoji.",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "/whats-on/2018/event/conservatory",
+    name: "The Barbican Conservatory",
+    website:"https://www.barbican.org.uk/whats-on/2018/event/conservatory",
+    address:"Barbican Centre Silk Street, London EC2Y 8DS",
+    description:"Visit the second biggest Conservatory in London and explore our hidden tropical oasis in the heart of the city. Home to exotic fish and over 2,000 species of tropical plants and trees, a visit to our Conservatory is a perfect way to enjoy a lazy Sunday with friends and family. Did you know: the large concrete tower you'll see at the centre of the Conservatory is our Theatre flytower.",
+    category:['theme', 'garden'],
+},
+
+{
+    id: "Bermondsey-Arts-Club",
+    name: "Bermondsey Arts Club",
+    website:"http://bermondseyartsclub.co.uk/",
+    address:"102A Tower Bridge Rd  London  SE1 4TP ",
+    description:"The latest public lav to go through the process of hipsterfication is this art deco-style cocktail bar and music venue in Southwark. Run by art school graduate George Garnier.",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "The-Vault-of-Soho",
+    name: "The Vault of Soho",
+    website:"",
+    address:"3 Greek Street Soho London W1D 4NX",
+    description:"Chic bar hidden away behind a bookcase and offering a refined environment for classic cocktails.",
+    category:['bar', 'theme'],
+},
+
+{
+    id: "BOURNE-AND-HOLLINGSWORTH-BUILDINGS",
+    name: "BOURNE AND HOLLINGSWORTH BUILDINGS",
+    website:"http://www.bandhbuildings.com/",
+    address:"Bourne & Hollingsworth Buildings 42 Northampton Road Clerkenwell EC1R 0HU",
+    description:"The Bourne and Hollingsworth Buildings might look like an idyllic bar, but it's got a bit of a secret. Have a wonder around the bar, look behind a few pieces of furniture and you might stumble upon Below & Hidden, a secret basement bar based underneath the venue. Grab a cocktail and soak up everything this charming little venue has to offer.",
+    category:['', ''],
+},
+
+{
+    id: "BAR-ELBA",
+    name: "BAR ELBA",
+    website:"http://www.bar-elba.co.uk/",
+    address:"Rooftop, Mercury House 109-117 Waterloo Road London, SE1 8UL , opposite Waterloo station",
+    description:"Sitting on the rooftop of quirky bar Tonight Josephine, Bar Elba is a tropical island paradise (named after the Mediterranean island of the same name) complete with sunny colours, block lettering and a summer cocktail menu with over twenty different mixed drinks.",
+    category:['bar', 'outdoor'],
+},
+
+{
+    id: "pergola-paddington",
+    name: "PERGOLA",
+    website:"https://pergolalondon.com/pergola-paddington",
+    address:"5 Kingdom Street, Paddington Central, W2 6PY",
+    description:"Introducing drinking and dining into a new era…Our mission is to take unloved and unused spaces and turn them into unique, big and beautiful destinations ~ restaurants, bars, city skylines, atmosphere, music, seating for all and general good times – all under our iconic pergola.",
+    category:['bar', 'restaurant'],
+},
+
+{
+    id: "private-dining.html",
+    name: "Caboose",
+    website:"http://www.wearecaboose.com/private-dining.html",
+    address:"The Old Truman Brewery, Ely’s Yard, Brick Lane, E1 6QL",
+    description:"Caboose is a word for the last carriage in a train – and this Brick Lane restaurant is indeed located inside a derelict carriage that’s been restored. You book in advance for private cabin dining, for which there’s a set menu for £30 per head. It’s available for between 8 to 11 people (12 or 13 at a push) between 8pm – 11pm.",
+    category:['bar', 'restaurant'],
+},
+
+{
+    id: "Gremio-de-Brixton",
+    name: "Gremio de Brixton",
+    website:"http://gremiodebrixton.com/",
+    address:"Effra Rd, Brixton, London SW2 1JF",
+    description:"Tucked away in the crypt underneath St. Matthews Church, Gremio de Brixton is an authentic Spanish tapas restaurant and cocktail bar with resident DJs at weekends.",
+    category:['restaurant', 'bar'],
+},
+
+{
+    id: "Postman's-Park",
+    name: "Postman's Park",
+    website:"https://www.cityoflondon.gov.uk/things-to-do/green-spaces/city-gardens/visitor-information/Pages/Postman%27s-Park.aspx",
+    address:" King Edward Street, London EC1A 7BT",
+    description:"Postman’s Park gained its name due to its popularity as a lunch spot with workers from the nearby old General Post Office. Now it's home to the unique Watts memorial, a gallery of glazed tablets commemorating tragic acts of bravery. This corner of green space would be an even better kept secret it weren't for Jude Law's character visiting the park in the 2004 film Closer",
+    category:['garden', 'theme'],
+},
+
+
+
+]
+export  default Places ;
