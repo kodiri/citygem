b57f644f475d4b0ef632939fc81f9b3f601783aa
Vesna Brekalo <VesnaNew@vesnabrkalosMBP.localdomain>
2018-08-08 15:10:47 +0100

    Homepage background image #4 changes to homepage and added london skyline image

--- src/frontend/Components/HomePage/HomePage.js
+++ src/frontend/Components/HomePage/HomePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import london from './londonskyline.jpg';
+import london from '../Images/londonskyline.jpg';
 import './HomePage.css';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div className='header'>
-        <header className='image-background'>
+      <header className='header'>
+        <div className='image-background'>
           <img src={london} className='london-image' alt='london' />
           <h1 className='title-citygem'>Welcome to searching in citygem</h1>
-        </header>
-      </div>
+        </div>
+      </header>
     );
   }
 }
