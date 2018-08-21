12691709a736080b45ca19e670eabc00725deb17
Justin Liu <38537698+justinliu6165@users.noreply.github.com>
2018-08-21 18:17:02 +0100

    Changes to Gallery and Thumbnail + added synopsis in places.js (#36)
    * Changed the README file
    
    * Changes to the folder structure
    
    * Created the component js and css
    files
    
    * Create a Thumbnail Component #9
    
    * Change rem to % for the width of the thuhmbnail
    
    * Create a Gallery Component #10
    
    * Include navbar #5 plus styling
    
    * Create Image Slider Component #13
    
    * Include a Search Bar (design only) #6
    
    * deleted serach bar component
    
    * added synopsis and some changes to gallery and thumbnails (incomplete)

--- src/frontend/Components/Thumbnail/Thumbnail.js
+++ src/frontend/Components/Thumbnail/Thumbnail.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './Thumbnail.css';
 
 export default function Thumbnail(props) {
-    let imageUrl = require(`../Images/${props.id}.jpg`);
+
     
     return (
-        <div className="thumbnail-container" 
-            style={{ backgroundImage : `url(${imageUrl})` }}>
-            <h3>{props.name}</h3>
+        <div className="thumbnail-container">
+            <a href="/details">
+                <img alt={props.name} src={require(`../Images/${props.id}.jpg`)}></img>
+            </a>
+            <div className="thumbnail-text">
+                <h5>{props.name}</h5>
+                <h4>{props.name}</h4>
+                <p>{props.name}</p>
+            </div>
         </div>
     )
 }
+
+//let imageUrl = require(`../Images/${props.id}.jpg`);
+// style={{ backgroundImage : `url(${imageUrl})` }}>
+//             <h3>{props.name}</h3>
